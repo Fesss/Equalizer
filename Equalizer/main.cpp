75b1c513bd636ef121f6e3dc28b48c45eb93ab8b
Vlasova Elena <helenavlasova39@gmail.com>
2013-12-09 22:14:06 +0400

    Organize commit

--- Equalizer/main.cpp
+++ Equalizer/main.cpp
@@ -1,20 +1,45 @@
-/*
- * File:   main.cpp
- * Author: Ryazantsev
- *
- * Created on November 12, 2013, 6:45 PM
- */
 
 #include <QtGui/QApplication>
+#include <winsock2.h>
+#include <iostream>
+#include "QWidget"
+#include "QVBoxLayout"
+#include "QSlider"
+#include "QTimer"
+#include "QSound"
+#include "Sound.h"
+#include "Walkman.h"
 
 int main(int argc, char *argv[]) {
     // initialize resources, if needed
     // Q_INIT_RESOURCE(resfile);
-
+   
     QApplication app(argc, argv);
+    /*
+    MainWindow *mw = new MainWindow();
+    mw->show();
+    mw->timerStart();
+*/
+    //QSound *snd = new QSound("03.wav");
+   // Sound *snd = new Sound("03.wav");
+   // std::cout<<snd->getLength();
+    // snd->play("03.wav");
+    /*    QWidget *box = new QWidget();
+    WalkmanView *wv = new WalkmanView();
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addLayout(wv,100);
+    box->setLayout(layout);
+    box->show(); 
 
+    QTimer *tm = new QTimer();
+    QObject::connect(tm,SIGNAL(timeout()),wv,SLOT(sheck()));
+    tm->start(100);
+    */
+    
     // create and show your widgets here
-
-    return app.exec();
+    Walkman *wm = new Walkman();
+    wm->show();
+    app.exec();
+   return 1;
 }
 
